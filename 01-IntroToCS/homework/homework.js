06adf5d9eeda9c9e12d63e80f491ee07fd71448c
JorgeVargasW <jorgevargasfeliciano30@gmail.com>
2022-06-13 20:29:25 -0500

    first

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,7 +2,14 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  sum = 0
+  var numReverse = num.split('').reverse().join('')
+  
+  for (i = 0; i < numReverse.length; i++) {
+   console.log(`${sum} = ${sum}+${numReverse[i]} * 2 ** ${i} `)
+    sum = sum + numReverse[i] * 2 ** i;
+  }
+  return sum;
 }
 
 function DecimalABinario(num) {
