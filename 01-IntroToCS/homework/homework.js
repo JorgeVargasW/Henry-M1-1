2f7c14b98fb9bcbdf7ff847e2425bab5330315c8
JorgeVargasW <jorgevargasfeliciano30@gmail.com>
2022-06-14 13:22:20 -0500

    Solucionado

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,19 +2,28 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  sum = 0
-  var numReverse = num.split('').reverse().join('')
-  
-  for (i = 0; i < numReverse.length; i++) {
-   console.log(`${sum} = ${sum}+${numReverse[i]} * 2 ** ${i} `)
-    sum = sum + numReverse[i] * 2 ** i;
-  }
-  return sum;
+  const binary = String(num);
+	let exponent = 0;
+	let decimalNumber = 0;
+
+	for (let i = binary.length; i > 0; i--) {
+		if (binary[i - 1] === '1') {
+			decimalNumber += 2 ** exponent;
+		}
+		exponent += 1;
+	}
+	return decimalNumber;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let binario = '';
+	let newNum = num;
+	while (newNum > 0) {
+		binario += newNum % 2;
+		newNum = Math.floor(newNum / 2);
+	}
+	return binario.split('').reverse().join('');
 }
 
 
